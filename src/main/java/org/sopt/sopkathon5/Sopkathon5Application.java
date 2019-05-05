08727103ea21e5d3da66951ee7059113702d872b
songz1 <21pori30@gmail.com>
2019-05-05 20:17:20 +0900

    new project

---
+++ src/main/java/org/sopt/sopkathon5/Sopkathon5Application.java
@@ -0,0 +1,13 @@
+package org.sopt.sopkathon5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sopkathon5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sopkathon5Application.class, args);
+	}
+
+}
---
+++ src/main/java/org/sopt/sopkathon5/utils/ResponseMessage.java
@@ -0,0 +1,28 @@
+package org.sopt.sopkathon5.utils;
+
+public class ResponseMessage {
+
+	public static final String READ_USER = "회원 정보 조회 성공";
+	public static final String READ_GROUP_USER = "그룹 회원 조회 성공";
+	public static final String NOT_FOUND_USER = "회원을 찾을 수 없습니다.";
+	public static final String CREATED_USER = "회원 가입 성공";
+	public static final String UPDATE_USER = "회원 정보 수정 성공";
+	public static final String DELETE_USER = "회원 탈퇴 성공";
+	public static final String LOGIN_SUCCESS = "로그인 성공";
+	public static final String LOGIN_FAIL = "로그인 실패";
+	public static final String DUPLICATED_ID = "아이디 중복";
+	public static final String AVALIABLE_ID = "사용할 수 있는 아이디입니다.";
+
+	public static final String NOT_FOUND_PW = "비밀번호가 틀렸습니다.";
+	public static final String PW_SUCCEESS = "비밀번호가 일치 합니다.";
+	public static final String UPDATE_PW = "비밀번호 수정 성공";
+
+	public static final String UNAUTHORIZED = "권한이 없습니다.";
+
+	public static final String NULL_POINTER = "NULL 에러";
+
+	public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
+
+	public static final String DB_ERROR = "데이터베이스 에러";
+
+}
